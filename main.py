696425c8d956a96e5e300a4072e7a8d6f1e2d6da
Goshik92 <igorsemenov@google.com>
2023-09-22 01:10:49 +0000

    Forward args to memtester
    Signed-off-by: Goshik92 <igorsemenov@google.com>

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 import ocptv.output as tv
+import argparse
 import sh
 import re
 from memtester_parsing import MemtesterObserver
+from contextlib import redirect_stderr
 
 def main():
+    parser = argparse.ArgumentParser(description="OCP-compliant memtester wrapper.")
+    parser.add_argument("--mt_path", default="memtester",
+        help="Path to memtester executable")
+    parser.add_argument("--mt_args", default="",
+        help="Memtester arguments packed into a string. See `man memtester` for more detail")
+    args = parser.parse_args()
+
     run = tv.TestRun(name="memtester", version="1.0")
     dut = tv.Dut(id=sh.hostid().strip(), name=sh.hostname().strip())
     
@@ -48,10 +57,10 @@ def main():
             
             # Report diagnosis
             def run_callback(failed_loop_count):
-                if failed_loop_count > 0:
-                    step.add_diagnosis(tv.DiagnosisType.FAIL, verdict="memtester-failed")
-                else:
+                if failed_loop_count == 0:
                     step.add_diagnosis(tv.DiagnosisType.PASS, verdict="memtester-passed")
+                else:
+                    step.add_diagnosis(tv.DiagnosisType.FAIL, verdict="memtester-failed")
             observer.callbacks.run_ready = run_callback
             
             # Report parsing errors (supposed to only happen with unknown memtester versions)
@@ -60,12 +69,19 @@ def main():
             observer.callbacks.parsing_error = parsing_error_callback
             
             # Run memtester (finally!)
-            try:
-                # TODO: forward command line parameters to memtester
-                observer.run(sh.memtester("100m", 3, _iter=True))
+            try:  
+                mt_cmd = sh.Command(args.mt_path)
+                mt_args = args.mt_args.split(" ")
+                with redirect_stderr(None): # Magic to silence sh lib
+                    # For _ok_code see https://linux.die.net/man/8/memtester
+                    observer.run(mt_cmd(*mt_args, _iter=True, _ok_code=(0, 2, 4)))
             except sh.CommandNotFound as e:
-                step.add_log(tv.LogSeverity.ERROR, "Memtester not found: {}".format(e))
-                step.add_diagnosis(tv.DiagnosisType.FAIL, verdict="memtester-not-found")
+                m = "Memtester not found: {}".format(e)
+                step.add_error(symptom="memtester-not-found", message=m)
+            except sh.ErrorReturnCode as e:
+                m = "Memtester returned code {}".format(e.exit_code)
+                step.add_error(symptom="memtester-error-code", message=m)
+                step.add_log(tv.LogSeverity.ERROR, "Memtester error description: {}".format(e))
             
             bad_addrs.end()
                
